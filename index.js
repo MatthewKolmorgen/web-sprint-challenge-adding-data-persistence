03357fc7765d2be314222c3ee8913e2380a1077c
Matthew <mattk.development@gmail.com>
2021-01-10 09:54:54 -0600

    mvp

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // start your server here
+onst express = require('express')
+
+const server = express()
+
+const projectRouter = require('./routers/projectRouter')
+
+const resourceRouter = require('./routers/resourceRouter')
+
+const taskRouter = require('./routers/taskRouter')
+
+server.use(express.json())
+
+const port = process.env.PORT || 6002
+
+server.use("/api/projects", projectRouter)
+
+server.use('/api/resources', resourceRouter)
+
+server.use('/api/tasks', taskRouter)
+server.get("/", (req, res) => {
+    res.status(200).json({ API: "RUNNING" });
+  });
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`API RUNNING ON PORT ${PORT}`));
